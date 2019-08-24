06e785dc0623be2a35d8bb7240cb93142899589a
Filmon habtu <filsolu93@gmail.com>
2019-08-23 20:25:57 -0700

    work hard for bargar

---
+++ server.js
@@ -0,0 +1,48 @@
+// Packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var path = require('path');
+var exphbs = require("express-handlebars");
+
+// Express
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Routes
+var routes = require("./routes/burgerRoutes");
+
+// DB Models
+var db = require('./models');
+
+// Handlebars
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+// Method-override - Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+// listen on port 3000
+db.sequelize.sync().then(function () {
+    app.listen(port, function (err) {
+        console.log('Listening on port ' + port);
+    });
+});
